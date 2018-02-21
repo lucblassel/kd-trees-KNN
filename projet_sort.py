def9a7da2498783705e0f4f12b6f3c20ce59cee1
luc blassel <luc.blassel@gmail.com>
2018-02-21 20:30:18 +0100

    added main

--- projet_sort.py
+++ projet_sort.py
@@ -3,17 +3,18 @@ Different sorting functions
 authors: Luc Blassel, Romain Gautron
 """
 
- def shellSort(array,dim):
-     "Shell sort using Shell's (original) gap sequence: n/2, n/4, ..., 1."
-     gap = len(array) // 2
-     # loop over the gaps
-     while gap > 0:
-         # do the insertion sort
-         for i in range(gap, len(array)):
-             val = array[i][dim]
-             j = i
-             while j >= gap and array[j - gap][dim] > val:
-                 array[j][dim] = array[j - gap][dim]
-                 j -= gap
-             array[j][dim] = val
-         gap //= 2
+def shellSort(array,dim):
+    "Shell sort using Shell's (original) gap sequence: n/2, n/4, ..., 1."
+    gap = len(array) // 2
+    # loop over the gaps
+    while gap > 0:
+        # do the insertion sort
+        for i in range(gap, len(array)):
+            val = array[i][dim]
+            j = i
+            while j >= gap and array[j - gap][dim] > val:
+                array[j][dim] = array[j - gap][dim]
+                j -= gap
+            array[j][dim] = val
+        gap //= 2
+    return array
