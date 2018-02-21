77eb976d8791b53a27b047c50c3e0bc2e892ef5c
luc blassel <luc.blassel@gmail.com>
2018-02-21 19:19:12 +0100

    consolidated sort files

--- projet_sort.py
+++ projet_sort.py
@@ -1,3 +1,8 @@
+"""
+Different sorting functions
+authors: Luc Blassel, Romain Gautron
+"""
+
  def shellSort(array,dim):
      "Shell sort using Shell's (original) gap sequence: n/2, n/4, ..., 1."
      gap = len(array) // 2
