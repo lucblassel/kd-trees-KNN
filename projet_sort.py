178cec5b5d72fa50b47faaa93b00b90af42104d5
romaingautronapt <Romain@Romains-MacBook-Air.local>
2018-02-22 14:18:05 +0100

    sorting fixed

--- projet_sort.py
+++ projet_sort.py
@@ -1,14 +1,16 @@
- def shellSort(array,dim):
-     "Shell sort using Shell's (original) gap sequence: n/2, n/4, ..., 1."
-     gap = len(array) // 2
-     # loop over the gaps
-     while gap > 0:
-         # do the insertion sort
-         for i in range(gap, len(array)):
-             val = array[i][dim]
-             j = i
-             while j >= gap and array[j - gap][dim] > val:
-                 array[j][dim] = array[j - gap][dim]
-                 j -= gap
-             array[j][dim] = val
-         gap //= 2
+x = [[1,2],[5,7],[-1,2]]
+def shellSort(array,dim):
+ "Shell sort using Shell's (original) gap sequence: n/2, n/4, ..., 1."
+ gap = len(array) // 2
+ # loop over the gaps
+ while gap > 0:
+     # do the insertion sort
+     for i in range(gap, len(array)):
+         val = array[i][dim]
+         sub_array = array[i]
+         j = i
+         while j >= gap and array[j - gap][dim] > val:
+             array[j] = array[j - gap]
+             j -= gap
+         array[j] = sub_array
+     gap //= 2
