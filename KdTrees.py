71f027657074bb2709699241c4387f0cdd1fdd90
luc blassel <luc.blassel@gmail.com>
2018-02-22 11:47:51 +0100

    changed sorting, aded tree printing

--- KdTrees.py
+++ KdTrees.py
@@ -5,7 +5,7 @@ authors: Luc Blassel, Romain Gautron
 
 from projet_sort import *
 from random import randint
-import sys
+import operator
 
 class Node:
     """
@@ -23,11 +23,30 @@ class Node:
     def hasChildren(self):
         return False if self.right == None and self.left == None else True
 
+    def __str__(self, depth=0):
+        """
+        modified snippet of Steve Krenzel
+        """
+        dim = len(self.value)
+        ret = ""
+
+        # Print right branch
+        if self.right != None:
+            ret += self.right.__str__(depth + 1)
+
+        # Print own value
+        ret += "\n" + ("    "*depth*dim) + str(self.value)
+
+        # Print left branch
+        if self.left != None:
+            ret += self.left.__str__(depth + 1)
+
+        return ret
+
 def createTree(pointList,dimensions,depth=0,parent=None):
     """
     creates Kd-tree, pointsList is the list of points. dimensions is the dimension of the euclidean space in which these points are present (or number od fimensions along which you want to split the data). depth is the starting tree-depth
     """
-    print(pointList)
 
     if not pointList:
         return
@@ -37,7 +56,7 @@ def createTree(pointList,dimensions,depth=0,parent=None):
 
     axis = depth%dimensions #switch dimensions at each split
 
-    pointList = shellSort(pointList,axis)
+    pointList.sort(key=lambda point: point[axis]) 
     med = len(pointList)//2
 
     root = Node(value=pointList[med], parent=parent, axis=axis, visited=False)
@@ -46,15 +65,20 @@ def createTree(pointList,dimensions,depth=0,parent=None):
 
     return root
 
+
 def genCloud(num,dims, min, max):
     return [[randint(min,max) for i in range(dims)] for j in range(num)]
 
 def main():
-    num = 10
+    num = 30
     dims = 2
-    min = -5
-    max = 5
+    min = -10
+    max = 10
     cloud = genCloud(num,dims,min,max)
+
+    #example set from https://gopalcdas.com/2017/05/24/construction-of-k-d-tree-and-using-it-for-nearest-neighbour-search/ (FOR TESTING)
+    cloud = [[1, 3],[1, 8], [2, 2], [2, 10], [3, 6], [4, 1], [5, 4], [6, 8], [7, 4], [7, 7], [8, 2], [8, 5],[9, 9]]
+    print(cloud)
 
     tree = createTree(pointList=cloud,dimensions=dims)
     print(tree)
